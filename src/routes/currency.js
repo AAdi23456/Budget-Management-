a772b91ab774c87b92f1f30c0e5f1201aee4fe6f
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2024-05-27 20:44:20 +0530

    done

--- src/routes/currency.js
+++ src/routes/currency.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const { getCurrencyConversion } = require('../controllers/currencyController');
-const authenticate = require('../middleware/authenticate');
 const router = express.Router();
 
-router.use(authenticate);
 
 router.get('/convert', getCurrencyConversion);
 
